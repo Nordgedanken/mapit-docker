ed4502fc467b2b392086b51ce3df1eac6f8fbca0
Marcel <MTRNord@users.noreply.github.com>
2016-08-23 20:27:15 +0200

    Update install-site.sh

--- install-site.sh
+++ install-site.sh
@@ -165,7 +165,7 @@ echo "$HOST" > "$OLD_HOST_FILE"
 
 REPOSITORY="$DIRECTORY/$SITE"
 
-REPOSITORY_URL=git://github.com/mysociety/$SITE.git
+REPOSITORY_URL=git://github.com/Nordgedanken/$SITE.git
 BRANCH=master
 
 DISTRIBUTION="$(lsb_release -i -s  | tr A-Z a-z)"
